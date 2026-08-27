fe75d9c8602d728e367a8d1eb99a80c8f107b691
aymen ELFADILI <aitabbou111@gmail.com>
2026-08-27 19:40:54 +0100

    fixes

---
+++ src/main/java/com/example/demo/controller/SwaggerRedirectController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SwaggerRedirectController {
+
+    @GetMapping("/swagger-ui")
+    public String redirectToSwaggerUi() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
